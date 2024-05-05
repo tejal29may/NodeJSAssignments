c2ba683866fbe552eee18ee38ea354ef8fd7e42f
tejal29may <85352094+tejal29may@users.noreply.github.com>
2024-05-05 14:48:47 +0530

    2nd assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+let fs=require("fs");
+
+fs.appendFile("textFile.txt","hello this is the list here",function(err){
+    if(err){
+        throw err;
+    }else{
+       console.log("file Created succesfully!");
+    }
+})
+
+fs.appendFile("textFile.txt","1. what is you name? \n 2. where are you from \n 3. okk goood ",function(err){
+    if(err){
+        throw err;
+    }else{
+       console.log("task added  succesfully!");
+    }
+})
+
+fs.readFile("textFile.txt", "utf8", function(err, data) {
+    if (err) {
+        throw err;
+    } else {
+        console.log("File content:");
+        console.log(data);
+    }
+});
+
+fs.unlink('textFile.txt', function (err) {
+    if (err) throw err;
+    console.log('File deleted!');
+  });
